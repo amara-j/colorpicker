b3b2e0b541a7e58b7562efd8318a824a5614a687
Amara Jaeger <amara.jaeger@gmail.com>
2021-01-27 13:47:19 -0500

    add checkboxes to lock colors, only randomize on click if not checked

--- src/ColorBlock.js
+++ src/ColorBlock.js
@@ -3,18 +3,25 @@ import {React, useState} from 'react';
 const ColorBlock = () => {
 
 const [color, updateColor] = useState(`rgb(${Math.floor(255*Math.random())},${Math.floor(255*Math.random())},${Math.floor(255*Math.random())})`)
+const [isPinned, updatePin] = useState(false);
 
 const randomizeColor = () => {
+    if (!isPinned) {
     const randomRed = Math.floor(255*Math.random());
     const randomGreen = Math.floor(255*Math.random());
     const randomBlue = Math.floor(255*Math.random());
     const randomColor = `rgb(${randomRed},${randomGreen},${randomBlue})`
     updateColor(randomColor)
-    console.log(randomColor)
+}}
+
+const handlePinChange = () => {
+    updatePin(!isPinned)
 }
+
     return (
-        <div className = "ColorBlock">
-<div onClick = {() => randomizeColor()} style={{backgroundColor: color, width: "100%", height: "700px", textAlign: "center"}}></div>
+    <div className = "ColorBlock">
+    <div onClick = {() => randomizeColor()} style={{backgroundColor: color, width: "100%", height: "700px", textAlign: "center"}}></div>
+   <input type = "checkbox" onClick = {() => handlePinChange()}></input>
    </div>
     );
 }
