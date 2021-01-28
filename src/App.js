bd65fab7d3ef1584426945a2f5a6e4f824760cef
Amara Jaeger <amara.jaeger@gmail.com>
2021-01-28 15:58:29 -0500

    click hex or rgb code to copy to clipboard

--- src/App.js
+++ src/App.js
@@ -1,37 +1,57 @@
-import './App.css';
-import {React, useState} from 'react';
-import ColorBlock from './ColorBlock.js'
+import "./App.css";
+import { React, useState } from "react";
+import ColorBlock from "./ColorBlock.js";
 
 const ColorBlockContainer = () => {
+  const [blockPinState, updatePin] = useState([false, false, false, false]);
 
-const [blockPinState, updatePin] = useState([false, false, false, false]);
-
-const setBlockPinned = (index, isPinned) => {
-    console.log(index, isPinned)
+  const setBlockPinned = (index, isPinned) => {
+    console.log(index, isPinned);
     let newBlockPinState = blockPinState;
     newBlockPinState[index] = isPinned;
     updatePin(newBlockPinState);
-    console.log(newBlockPinState)
-}
+    console.log(newBlockPinState);
+  };
 
-return (
-      <div className = "BlockContainer">
-    <ColorBlock id = "0" isPinned = {blockPinState[0]} onChange = {(index, isPinned)=> setBlockPinned(index,isPinned)}/>
-        <ColorBlock id = "1" isPinned = {blockPinState[1]} onChange = {(index, isPinned)=> setBlockPinned(index,isPinned)}/>
-            <ColorBlock id = "2" isPinned = {blockPinState[2]} onChange = {(index, isPinned)=> setBlockPinned(index,isPinned)}/>
-                <ColorBlock id = "3" isPinned = {blockPinState[3]} onChange = {(index, isPinned)=> setBlockPinned(index,isPinned)}/>
+  return (
+    <div className="BlockContainer">
+      <ColorBlock
+        id="0"
+        isPinned={blockPinState[0]}
+        onChange={(index, shouldBePinned) =>
+          setBlockPinned(index, shouldBePinned)
+        }
+      />
+      <ColorBlock
+        id="1"
+        isPinned={blockPinState[1]}
+        onChange={(index, shouldBePinned) =>
+          setBlockPinned(index, shouldBePinned)
+        }
+      />
+      <ColorBlock
+        id="2"
+        isPinned={blockPinState[2]}
+        onChange={(index, shouldBePinned) =>
+          setBlockPinned(index, shouldBePinned)
+        }
+      />
+      <ColorBlock
+        id="3"
+        isPinned={blockPinState[3]}
+        onChange={(index, shouldBePinned) =>
+          setBlockPinned(index, shouldBePinned)
+        }
+      />
     </div>
-)
-}
+  );
+};
 
 const App = () => {
-  return (
-<ColorBlockContainer/>
-  );
-}
+  return <ColorBlockContainer />;
+};
 
-
-//colorBlock accepts boolean for checked/not and a function to run 
+//colorBlock accepts boolean for checked/not and a function to run
 //
 
 export default App;
--- src/ColorBlock.js
+++ src/ColorBlock.js
@@ -1,47 +1,69 @@
-import {React, useState, useEffect} from 'react';
+import { React, useState, useEffect } from "react";
 
 const ColorBlock = (props) => {
+  const [colorRGB, updateColorRGB] = useState([
+    Math.floor(255 * Math.random()),
+    Math.floor(255 * Math.random()),
+    Math.floor(255 * Math.random()),
+  ]);
 
-const [colorRGB, updateColorRGB] = useState([Math.floor(255*Math.random()),Math.floor(255*Math.random()),Math.floor(255*Math.random())]);
-
-const randomizeColor = () => {
-    const randomRed = Math.floor(255*Math.random());
-    const randomGreen = Math.floor(255*Math.random());
-    const randomBlue = Math.floor(255*Math.random());
-    const randomRGB = [randomRed,randomGreen,randomBlue];
+  const randomizeColor = () => {
+    const randomRed = Math.floor(255 * Math.random());
+    const randomGreen = Math.floor(255 * Math.random());
+    const randomBlue = Math.floor(255 * Math.random());
+    const randomRGB = [randomRed, randomGreen, randomBlue];
     const randomHex = rgbToHex(randomRGB);
-    updateColorRGB(randomRGB)
-    updateColorHex(randomHex)}
+    updateColorRGB(randomRGB);
+    updateColorHex(randomHex);
+  };
 
-const rgbToHex = (rgbArray) => {
-  let hex = '#';
-  rgbArray.forEach(element => 
-    {const elementString = element.toString(16);
-    var hexComponent= elementString.length === 1 ? "0" + elementString : elementString;
-    hex += hexComponent})
-    return hex
-}
+  const rgbToHex = (rgbArray) => {
+    let hex = "#";
+    rgbArray.forEach((element) => {
+      const elementString = element.toString(16);
+      var hexComponent =
+        elementString.length === 1 ? "0" + elementString : elementString;
+      hex += hexComponent;
+    });
+    return hex;
+  };
 
-const [colorHex, updateColorHex] = useState(rgbToHex(colorRGB))
+  const [colorHex, updateColorHex] = useState(rgbToHex(colorRGB));
 
-useEffect(()=> {
-  document.addEventListener('keydown', randomizeColor)
-}, [])
+  useEffect(() => {
+    document.addEventListener("keydown", randomizeColor);
+  }, []);
 
-const handleCheckBoxChange = (e) => {
-props.onChange(props.id, e.target.checked)
-  console.log(e.target.checked)
-}
+  const handleCheckBoxChange = (e) => {
+    props.onChange(props.id, e.target.checked);
+    console.log(e.target.checked);
+  };
 
-    return (
-    <div className = "ColorBlock">
-    <div className = "ColorBlockContent" style={{backgroundColor: `rgb(${colorRGB})`}}>
-{['rgb: '+  colorRGB.join(', '), 'hex: '+colorHex].join("\n \n")} 
-    <input type = "checkbox" onChange = {(e) => handleCheckBoxChange(e)} checked = {props.isPinned}></input>
-    </div>
-   </div>
+  const clipboardCopy = (textToCopy) => {
+    navigator.clipboard.writeText(textToCopy).then(
+      function () {
+        /* clipboard success message/action*/
+      },
+      function () {
+        /* clipboard failure message/action*/
+      }
     );
-}
+  };
+
+  return (
+    <div className="ColorBlock" style={{ backgroundColor: `rgb(${colorRGB})` }}>
+      <p onClick={() => clipboardCopy(colorRGB)}>
+        {"rgb: " + colorRGB.join(",")}
+      </p>
+      <p onClick={() => clipboardCopy(colorHex)}> {"hex: " + colorHex}</p>
+      <input
+        type="checkbox"
+        onChange={(e) => handleCheckBoxChange(e)}
+        checked={props.isPinned}
+      ></input>
+    </div>
+  );
+};
 
 //randomize color instead makes an rgb array of 4 random colors
 // for any component not checked, it updates color
